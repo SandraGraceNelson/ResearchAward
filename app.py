726307348923f65497c316eeb5e98628ea09432f
Sandra Grace Nelson <100516887+SandraGraceNelson@users.noreply.github.com>
2023-04-17 11:47:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from textblob import TextBlob
+
+def analyze_text(text):
+    blob = TextBlob(text)
+    sentiment_polarity = blob.sentiment.polarity
+    if sentiment_polarity <= 0:
+        return "The text contains language associated with depression."
+    else:
+        return "The text does not contain language associated with depression."
+
+def run_app():
+    # Set page width and page title
+    st.set_page_config(page_title="Depression Analysis App", page_icon=":smiley:", layout="wide")
+    
+    # Set background image
+    page_bg = '''
+    <style>
+    body {
+        background-image: url("https://i.imgur.com/dyxRJ6g.png") !important;
+        background-size: cover;
+        background-position: center;
+    }
+    </style>
+    '''
+    st.markdown(page_bg, unsafe_allow_html=True)
+
+    # Set app title and subtitle
+    st.title("Depression Analysis App")
+    st.markdown("*Analyze text to determine if it contains language associated with depression.*")
+
+    # Create text input and analyze button
+    text = st.text_area("Enter your text:", height=150)
+    if st.button("Analyze"):
+        result = analyze_text(text)
+        st.write(result)
+
+if __name__ == "__main__":
+    run_app()
